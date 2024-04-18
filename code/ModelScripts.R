a9956512026b8dbf7b743944cff85ff2af8f4f2f
phillipriv9 <priver02@villanova.edu>
2024-04-18 09:04:10 -0400

    Added Blue Nitrogen Page

---
+++ code/ModelScripts.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(tidyverse)
+library(lme4)
+
+
+AllCombined$Latitude <- as.numeric(AllCombined$Latitude)
+
+df1 <- AllCombined
+
+df2 <- df1[! (is.infinite(df1$CN) | is.na(df1$CN)), ]
+
+
+df3 <- df2 %>%
+  filter(Habitat %in% c("marsh", "mangrove"), CN > 5, CN < 100)
